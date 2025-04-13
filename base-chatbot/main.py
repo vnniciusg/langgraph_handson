f7fe9ea69fc95b8af470d6d2010fd1bb036dfbb8
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-04-13 08:52:48 -0300

    feat: add memory

--- base-chatbot/main.py
+++ base-chatbot/main.py
@@ -10,6 +10,7 @@ from typing import Annotated, TypedDict
 from langgraph.graph.message import add_messages
 from langgraph.graph import START, END, StateGraph
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_core.messages import AnyMessage, ToolMessage
 from langchain_community.tools.tavily_search import TavilySearchResults
 
@@ -36,65 +37,25 @@ def chatbot(state: State) -> dict:
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
-class BasicToolNode:
-    def __init__(self, tools: list) -> None:
-        self.tools_by_name = {tool.name: tool for tool in tools}
-
-    def __call__(self, inputs: dict):
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("no message found in input")
-
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(tool_call["args"])
-            outputs.append(ToolMessage(content=json.dumps(tool_result), name=tool_call["name"], tool_call_id=tool_call["id"]))
-        return {"messages": outputs}
-
-
-def route_tools(state: State):
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(f"No messages found in input state to tool_edge: {state}")
-
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-
-    return END
-
-
-tool_node = BasicToolNode(tools=[tool])
+tool_node = ToolNode(tools=[tool])
 
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_node("tools", tool_node)
 
 graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
-graph_builder.add_conditional_edges("chatbot", route_tools, {"tools": "tools", END: END})
+graph_builder.add_conditional_edges("chatbot", tools_condition)
 
 memory = MemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
 
 
-def stream_graph_updates(user_input: str) -> None:
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-
-
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
-        stream_graph_updates(user_input)
-    except:
-        user_input = "What do you know about LangGraph?"
-        print("User: ", user_input)
-        stream_graph_updates(user_input)
-        break
+inputs = ["Hi there! My name is Will.", "Remember my name?"]
+for user_input in inputs:
+    events = graph.stream(
+        {"messages": [{"role": "user", "content": user_input}]},
+        {'configurable': {'thread_id': "1"}},
+        stream_mode="values",
+    )
+    for event in events:
+        event["messages"][-1].pretty_print()
