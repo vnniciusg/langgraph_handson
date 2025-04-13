9ad82b1fc597d6890f1565aa0f4e380f7f26f137
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-04-13 09:10:19 -0300

    feat: add customized state

--- base-chatbot/main.py
+++ base-chatbot/main.py
@@ -7,9 +7,9 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from langchain_openai import ChatOpenAI
 from typing import Annotated, TypedDict
-from langchain_core.tools import tool
+from langchain_core.tools import InjectedToolCallId, tool
 from langgraph.graph.message import add_messages
-from langchain_core.messages import AnyMessage
+from langchain_core.messages import AnyMessage, ToolMessage
 from langgraph.graph import START, END, StateGraph
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import ToolNode, tools_condition
@@ -28,10 +28,22 @@ graph_builder = StateGraph(State)
 
 
 @tool
-def human_assistance(query: str) -> str:
+def human_assistance(name: str, birthday: str, tool_call_id: Annotated[str, InjectedToolCallId]) -> str:
     """Request assistance from a human"""
-    human_response = interrupt({"query": query})
-    return human_response["data"]
+    human_response = interrupt({"question": "Is this correct?", "name": name, "birthday": birthday})
+
+    if human_response.get("correct", "").lower().startswith("y"):
+        verified_name = name
+        verified_birthday = birthday
+        response = "Correct"
+    else:
+        verified_name = human_response.get("name", name)
+        verified_birthday = human_response.get("birthday", birthday)
+        response = f"Made a correction: {human_response}"
+
+    state_update = {"name": verified_name, "birthday": verified_birthday, "messages": [ToolMessage(response, tool_call_id=tool_call_id)]}
+
+    return Command(update=state_update)
 
 
 config = Settings()
@@ -44,7 +56,7 @@ llm_with_tools = llm.bind_tools(tools)
 
 def chatbot(state: State) -> dict:
     message = llm_with_tools.invoke(state["messages"])
-    assert (len(message.tool_calls) <= 1)
+    assert len(message.tool_calls) <= 1
     return {"messages": [message]}
 
 
@@ -60,13 +72,24 @@ graph_builder.add_conditional_edges("chatbot", tools_condition)
 memory = MemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
 
-user_input = "I need some expert guidance for building an AI agent. Could you request assistance for me?"
-config = {"configurable": {"thread_id": "1"}}
+user_input = "Can you look up when LangGraph was released? When you have the answer, use the human_assistance tool for review."
 events = graph.stream(
     {"messages": [{"role": "user", "content": user_input}]},
-    config,
+    {"configurable": {"thread_id": "1"}},
     stream_mode="values",
 )
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
+
+human_command = Command(
+    resume={
+        "name": "LangGraph",
+        "birthday": "Jan 17, 2024",
+    },
+)
+
+events = graph.stream(human_command, {"configurable": {"thread_id": "1"}}, stream_mode="values")
 for event in events:
     if "messages" in event:
         event["messages"][-1].pretty_print()
